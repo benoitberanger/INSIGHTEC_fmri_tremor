d08dedbfae8b2c57ffbd57f45b1b5b44f314458d
Benoît Béranger <benoit.beranger@icm-institute.org>
2020-06-05 16:17:28 +0200

    added script for printing figures from SPM result UI

---
+++ step03_firstlevel_print_figures.m
@@ -0,0 +1,33 @@
+clear
+clc
+
+load e.mat
+
+model = e.getModel(cellstr2regex({'tapas_logpower','modulator'})).getPath';
+
+anat = e.getSerie('anat_T1_UNI').getVolume('^wmcs').getPath;
+anat = repmat(anat,size(model));
+
+coord = {
+    [ -1  -9  58]  'SMA_L'
+    [-36 -25  57]   'M1_L'
+    [-17 -23   6]  'VIM_L'
+    [  3 -68 -12] 'CB_V_R'
+    };
+
+contrast = {
+%     'Posture', 'Posture - Relax',...                       % boxcar
+    'reg', 'dreg', %'Posture-Relax',...                     % EMG_envelope_power_logpower
+    'Posture__power', 'Posture__power_diff',...            % modulator
+    };
+
+print_spm_figure( ...
+    'model',model, ...
+    'anat',anat, ...
+    'coord',coord ,...
+    'contrast',contrast,...
+    'correction','none',...
+    'threshold',0.001,...
+    'nvoxel', 3,...
+    'outdir',fullfile(pwd,'png'))
+
