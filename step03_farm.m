cf51e9652a3beb115f33572e1744ec78d2e8ed89
Benoît Béranger <benoit.beranger@icm-institute.org>
2021-04-24 00:03:08 +0200

    FARM : regressors are generated for both left & right arm whatever the number of ACC

--- step03_farm.m
+++ step03_farm.m
@@ -7,7 +7,6 @@ assert( ~isempty(which('ft_preprocessing')), 'FieldTrip library not detected. Ch
 assert( ~isempty(which('farm_rootdir'))    ,      'FARM library not detected. Check your MATLAB paths, or get : https://github.com/benoitberanger/FARM' )
 
 load e
-% e = e(end);
 
 fmri_volume = e.getSerie('run_nm').getVolume('^v').removeEmpty.getPath;
 fmri_volume = sort(fmri_volume);
@@ -28,6 +27,8 @@ fname_hdr = spm_file(filepath, 'ext', '.vhdr');
 fname_mrk = spm_file(filepath, 'ext', '.vmrk');
 
 %%
+
+t0 = tic;
 
 for iRun = 1 : length(filepath)
     %% Get file & sequence paramters
@@ -70,8 +71,8 @@ for iRun = 1 : length(filepath)
     
     % Plot
     % ft_databrowser(data.cfg, data)
-%     cfg.dataset
-%     [data.label num2cell(data.hdr.orig.impedances.channels)]
+    %     cfg.dataset
+    %     [data.label num2cell(data.hdr.orig.impedances.channels)]
     % continue
     
     %% ------------------------------------------------------------------------
@@ -83,13 +84,13 @@ for iRun = 1 : length(filepath)
     
     fname = farm.io.mat.get_fname(data, 'pca_clean');
     if exist(fname, 'file')
-        fprintf('skipping : %s \n', fname)
-        continue
+        fprintf('existing file %s \n', fname)
+        data = load(fname);
+    else
+        data = farm_main_workflow( data, emg_channel_regex );
+        farm_export_BVA(data)
+        farm_export_mat(data,[],1)
     end
-    data = farm_main_workflow( data, emg_channel_regex );
-    farm_export_BVA(data)
-    farm_export_mat(data,[],1)
-    continue
     
     
     %% Some plots
@@ -98,51 +99,118 @@ for iRun = 1 : length(filepath)
     figH = farm_plot_FFT(data,             'ACC',       'raw', [ 2   8],2); farm_print_figure( data, figH ); close(figH);
     
     
-    %% Time-Frequency Analysis
+    %% 1 ACC or 1 ACC ?
     
-    cfg_TFA = [];
-    cfg_TFA.emg_regex = emg_channel_regex;
-    cfg_TFA.acc_regex = '(^ACC)|(R_ACC)';
+    is_1_acc = any(strcmp( data.label, 'ACC_X'));
+    is_2_acc = any(strcmp( data.label, 'R_ACC_X'));
+    assert(xor(is_1_acc,is_2_acc),'wtf ?') % just to check
     
-    TFA = farm_time_frequency_analysis_emg_acc( data, cfg_TFA );
-    figH = farm_plot_TFA( data, TFA ); farm_print_figure( data, figH ); close(figH);
+    if is_1_acc
+        %% Time-Frequency Analysis
+        
+        cfg_TFA = [];
+        cfg_TFA.emg_regex = emg_channel_regex;
+        cfg_TFA.acc_regex = '^ACC';
+        
+        TFA = farm_time_frequency_analysis_emg_acc( data, cfg_TFA );
+        figH = farm_plot_TFA( data, TFA ); farm_print_figure( data, figH ); close(figH);
+        
+        
+        %% Coherence Analysis
+        
+        cfg_coh = [];
+        cfg_coh.emg_regex = emg_channel_regex;
+        cfg_coh.acc_regex = '^ACC';
+        
+        coh = farm_coherence_analysis_emg_acc( data, cfg_coh );
+        % ft_connectivityplot([], coh);
+        figH = farm_plot_coherence( data, coh ); farm_print_figure( data, figH ); close(figH);
+        
+        
+        %% Select best EMG channel, that matches ACC using coherence
+        
+        cfg_select_emg = [];
+        cfg_select_emg.emg_regex = emg_channel_regex;
+        cfg_select_emg.acc_regex = '^ACC';
+        
+        best_emg = farm_select_best_emg_using_acc_coherence( data, cfg_select_emg );
+        
+        
+        %% Generate regressors
+        
+        reginfo      = farm_make_regressor( data, best_emg.peakpower, best_emg.fsample);
+        reginfo.name = ['peakpower@bestemg==' best_emg.label];
+        figH         = farm_plot_regressor( data, reginfo ); farm_print_figure( data, figH ); close(figH);
+        farm_save_regressor(data, reginfo)
+        
+        
+        %% Accelerometer : this regressor will be a backup in case of bad EMG
+        
+        acc          = farm_get_timeseries(data,'^ACC','raw', [2 8],2);
+        reginfo      = farm_acc_regressor(data, acc);
+        reginfo.name = 'euclidiannorm@ACCXYZ_R';
+        figH         = farm_plot_regressor( data, reginfo ); farm_print_figure( data, figH ); close(figH);
+        farm_save_regressor(data, reginfo)
+        
+        
+    else
+        %% Time-Frequency Analysis
+        
+        cfg_TFA = [];
+        cfg_TFA.emg_regex = emg_channel_regex;
+        cfg_TFA.acc_regex = 'ACC';
+        
+        TFA = farm_time_frequency_analysis_emg_acc( data, cfg_TFA );
+        figH = farm_plot_TFA( data, TFA ); farm_print_figure( data, figH ); close(figH);
+        
+        
+        %% Coherence Analysis
+        
+        cfg_coh = [];
+        cfg_coh.emg_regex = emg_channel_regex;
+        cfg_coh.acc_regex = 'ACC';
+        
+        coh = farm_coherence_analysis_emg_acc( data, cfg_coh );
+        % ft_connectivityplot([], coh);
+        figH = farm_plot_coherence( data, coh ); farm_print_figure( data, figH ); close(figH);
+        
+        side = {'L', 'R'};
+        
+        for s = side
+            %% Select best EMG channel, that matches ACC using coherence
+            
+            LR = char(s);
+            
+            cfg_select_emg = [];
+            cfg_select_emg.emg_regex = sprintf('%s_(%s)',LR,emg_channel_regex);
+            cfg_select_emg.acc_regex = sprintf('%s_ACC' ,LR                  );
+            
+            best_emg = farm_select_best_emg_using_acc_coherence( data, cfg_select_emg );
+            
+            
+            %% Generate regressors
+            
+            reginfo      = farm_make_regressor( data, best_emg.peakpower, best_emg.fsample);
+            reginfo.name = ['peakpower@bestemg==' best_emg.label];
+            figH         = farm_plot_regressor( data, reginfo ); farm_print_figure( data, figH ); close(figH);
+            farm_save_regressor(data, reginfo)
+            
+            
+            %% Accelerometer : this regressor will be a backup in case of bad EMG
+            
+            acc          = farm_get_timeseries(data,sprintf('%s_ACC' ,LR),'raw', [2 8],2);
+            reginfo      = farm_acc_regressor(data, acc);
+            reginfo.name = sprintf('euclidiannorm@ACCXYZ_%s' ,LR);
+            figH         = farm_plot_regressor( data, reginfo ); farm_print_figure( data, figH ); close(figH);
+            farm_save_regressor(data, reginfo)
+            
+            
+        end % LR
+        
+    end % 1 or 2 acc ?
     
+    toc(t0);
     
-    %% Coherence Analysis
-    
-    cfg_coh = [];
-    cfg_coh.emg_regex = emg_channel_regex;
-    cfg_coh.acc_regex = '(^ACC)|(R_ACC)';
-    
-    coh = farm_coherence_analysis_emg_acc( data, cfg_coh );
-    % ft_connectivityplot([], coh);
-    figH = farm_plot_coherence( data, coh ); farm_print_figure( data, figH ); close(figH);
-    
-    
-    %% Select best EMG channel, that matches ACC using coherence
-    
-    cfg_select_emg = [];
-    cfg_select_emg.emg_regex = emg_channel_regex;
-    cfg_select_emg.acc_regex = '(^ACC)|(R_ACC)';
-    
-    best_emg = farm_select_best_emg_using_acc_coherence( data, cfg_select_emg );
-    
-    
-    %% Generate regressors
-    
-    reginfo      = farm_make_regressor( data, best_emg.peakpower, best_emg.fsample);
-    reginfo.name = ['peakpower@bestemg==' best_emg.label];
-    figH         = farm_plot_regressor( data, reginfo ); farm_print_figure( data, figH ); close(figH);
-    farm_save_regressor(data, reginfo)
-    
-    
-    %% Accelerometer : this regressor will be a backup in case of bad EMG
-    
-    acc          = farm_get_timeseries(data,'(^ACC)|(R_ACC)','raw', [2 8],2);
-    reginfo      = farm_acc_regressor(data, acc);
-    reginfo.name = 'euclidiannorm@ACCXYZ';
-    figH         = farm_plot_regressor( data, reginfo ); farm_print_figure( data, figH ); close(figH);
-    farm_save_regressor(data, reginfo)
-    
-    
-end
+end % run file
+
+toc(t0);
