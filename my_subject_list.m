7a871b5bdbeeed8b1677a692920c27d0e0e771b7
Benoît Béranger <benoit.beranger@icm-institute.org>
2021-04-26 15:32:10 +0200

    added 'my_subject_list' to have a global view of the cohort

---
+++ my_subject_list.m
@@ -0,0 +1,28 @@
+function data = my_subject_list()
+
+
+data = {
+    
+% subject ID     visite      sideACC    nRun    examName                                           channels
+'002-FJ'          'V1'         'R'       1      '2020_02_25_ULTRABRAIN_01_002_FJ_INCLUSION'       {'INSIGHTEC'   'FCR_L'   'ECR_L'                   'FCR_R'   'ECR_R'   'DEL_R'   'BIC_R'   'TRI_R'                                 'ACC_X'   'ACC_Y'   'ACC_Z'}
+'002-FJ'          'V3'         'R'       1      ''                                                {} % no FMRI
+'002-FJ'          'V4'         'R'       1      '2020_10_06_ULTRABRAIN_001_0002_FJ_V4'            {'INSIGHTEC'   'FCR_L'   'ECR_L'                   'FCR_R'   'ECR_R'   'DEL_R'   'BIC_R'   'TRI_R'                                 'ACC_X'   'ACC_Y'   'ACC_Z'}
+'002-FJ'          'V5'        'LR'       1      '2020_12_08_ULTRABRAIN_001_0002_FJ_V5'            {'INSIGHTEC' 'L_FCR'   'L_ECR'                   'R_FCR'   'R_ECR'   'R_DEL'   'R_BIC'   'R_TRI'   'L_ACC_X' 'L_ACC_Y' 'L_ACC_Z' 'R_ACC_X' 'R_ACC_Y' 'R_ACC_Z'}
+
+'003-PP'          'V1'         'R'       2      '2020_09_22_ULTRABRAIN_0003_PP_V1_INCLUSION_FMRI' {'INSIGHTEC'   'FCR_L'   'ECR_L'                   'FCR_R'   'ECR_R'   'DEL_R'   'BIC_R'   'TRI_R'                                 'ACC_X'   'ACC_Y'   'ACC_Z'}
+'003-PP'          'V3'        'LR'       1      '2020_11_03_ULTRABRAIN_001_003_PP_V3_E3'          {'INSIGHTEC' 'L_FCR'   'L_ECR'                   'R_FCR'   'R_ECR'   'R_DEL'   'R_BIC'   'R_TRI'   'L_ACC_X' 'L_ACC_Y' 'L_ACC_Z' 'R_ACC_X' 'R_ACC_Y' 'R_ACC_Z'}
+'003-PP'          'V4'        'LR'       1      '2020_11_17_ULTRABRAIN_001_003_PP_V4_control'     {'INSIGHTEC' 'L_FCR'   'L_ECR'                   'R_FCR'   'R_ECR'   'R_DEL'   'R_BIC'   'R_TRI'   'L_ACC_X' 'L_ACC_Y' 'L_ACC_Z' 'R_ACC_X' 'R_ACC_Y' 'R_ACC_Z'}
+'003-PP'          'V5'        'LR'       1      '2021_01_26_ULTRABRAIN_001_003_PP_V5_CONTROLE'    {'INSIGHTEC' 'L_FCR'   'L_ECR'                   'R_FCR'   'R_ECR'   'R_DEL'   'R_BIC'   'R_TRI'   'L_ACC_X' 'L_ACC_Y' 'L_ACC_Z' 'R_ACC_X' 'R_ACC_Y' 'R_ACC_Z'}
+
+'004-HJ'          'V1'        'LR'       1      '2021_01_19_ULTRABRAIN_001_0004_HJ_V1_INCLUSION'  {'INSIGHTEC' 'L_FCR'   'L_ECR'                   'R_FCR'   'R_ECR'   'R_DEL'   'R_BIC'   'R_TRI'   'L_ACC_X' 'L_ACC_Y' 'L_ACC_Z' 'R_ACC_X' 'R_ACC_Y' 'R_ACC_Z'}
+'004-HJ'          'V3'        'LR'       1      '2021_02_16_ULTRABRAIN_001_004_HJ_V3_CONTROLE'    {'INSIGHTEC' 'L_FCR'   'L_ECR'                   'R_FCR'   'R_ECR'   'R_DEL'   'R_BIC'   'R_TRI'   'L_ACC_X' 'L_ACC_Y' 'L_ACC_Z' 'R_ACC_X' 'R_ACC_Y' 'R_ACC_Z'}
+'004-HJ'          'V4'        'LR'       1      '2021_03_02_ULTRABRAIN_001_004_HJ_V4_CONTROLE'    {'INSIGHTEC' 'L_FCR'   'L_ECR'                   'R_FCR'   'R_ECR'   'R_DEL'   'R_BIC'   'R_TRI'   'L_ACC_X' 'L_ACC_Y' 'L_ACC_Z' 'R_ACC_X' 'R_ACC_Y' 'R_ACC_Z'}
+
+'005-BD'          'V1'        'LR'       1      '2021_02_02_ULTRABRAIN_001_005_BD_V1_CONTROL'     {            'L_FCR'   'L_ECR'   'L_BIC' 'L_TRI' 'R_FCR'   'R_ECR'   'R_DEL'   'R_BIC'   'R_TRI'   'L_ACC_X' 'L_ACC_Y' 'L_ACC_Z' 'R_ACC_X' 'R_ACC_Y' 'R_ACC_Z'}
+'005-BD'          'V3'        'LR'       1      '2021_03_30_ULTRABRAIN_001_005_BD_V3_CONTROLE'    {            'L_FCR'   'L_ECR'   'L_BIC' 'L_TRI' 'R_FCR'   'R_ECR'   'R_DEL'   'R_BIC'   'R_TRI'   'L_ACC_X' 'L_ACC_Y' 'L_ACC_Z' 'R_ACC_X' 'R_ACC_Y' 'R_ACC_Z'}
+
+'006-CJ'          'V1'        'LR'       1      '2021_04_06_ULTRABRAIN_001_006_CJ_V1_CONTROL'     {            'L_FCR'   'L_ECR'   'L_BIC' 'L_TRI' 'R_FCR'   'R_ECR'   'R_DEL'   'R_BIC'   'R_TRI'   'L_ACC_X' 'L_ACC_Y' 'L_ACC_Z' 'R_ACC_X' 'R_ACC_Y' 'R_ACC_Z'}
+
+};
+
+end % function
